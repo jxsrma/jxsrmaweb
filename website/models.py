598a8185e21ce73219286097379ef9a2dfb1aab5
Jash Sharma <jashsharma619@gmail.com>
2021-08-18 16:48:33 +0530

    Demo Submittion Working

--- website/models.py
+++ website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Demosubs(models.Model):
+    a_name = models.CharField(max_length=100)
+    a_email = models.CharField(max_length=100)
+    t_name = models.CharField(max_length=100)
+    t_url = models.CharField(max_length=100)
+    t_dis = models.TextField()
--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .models import Demosubs
 
 # Create your views here.
 
@@ -15,7 +17,22 @@ def shop(request):
     return render(request,'shop.html')
 
 def demo(request):
-    return render(request,'demo.html')
+
+    if request.method == 'POST':
+        artname = request.POST['artname']
+        email = request.POST['email']
+        trackname = request.POST['trackname']
+        trackurl = request.POST['trackurl']
+        infotext = request.POST['infotext']
+        print('success')
+
+        demoData = Demosubs(a_name = artname, a_email = email, t_name = trackname, t_url = trackurl, t_dis = infotext )
+        demoData.save()
+        return render(request,'demo.html')
+
+    else:    
+        return render(request,'demo.html')
+
 
 def cont(request):
     return render(request,'contact.html')
