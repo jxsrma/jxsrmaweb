9b5e7edfd0bbbcdbd2eecd6d6ee37621ece264b4
Jash Sharma <jashsharma619@gmail.com>
2021-09-07 12:04:16 +0530

    Dynamic Gallery Made

--- website/migrations/0002_imggallery.py
+++ website/migrations/0002_imggallery.py
@@ -16,7 +16,6 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('link', models.CharField(max_length=100)),
                 ('img', models.ImageField(upload_to='images')),
-                ('caption', models.CharField(max_length=100)),
             ],
         ),
     ]
