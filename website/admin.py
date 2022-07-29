6f82156037f5ca524386901a329e311739c3d346
Jash Sharma <jashsharma619@gmail.com>
2022-07-29 12:54:15 +0530

    ImportExport added on Admin Panel

--- website/admin.py
+++ website/admin.py
@@ -1,45 +1,28 @@
+from import_export.admin import ImportExportModelAdmin
 from django.contrib import admin
 from .models import Demosubs,ImgGallery,Contact, Released, EmailList,HomeScreenYT
 
 # Register your models here.
 
-# def download_csv(modeladmin, request, queryset):
-#     import csv
-#     f = open('Email List.csv', 'w')
-#     writer = csv.writer(f)
-#     writer.writerow(["name", "email", "active"])
-#     for s in queryset:
-#         writer.writerow([s.name, s.email, s.active])
-
-
 @admin.register(Demosubs)
-class demoSubAdmin(admin.ModelAdmin):
+class demoSubAdmin(ImportExportModelAdmin):
     list_display = ['a_name','t_name','t_url']
 
-
 @admin.register(ImgGallery)
-class imgGalleryAdmin(admin.ModelAdmin):
+class imgGalleryAdmin(ImportExportModelAdmin):
     list_display = ['link','imgLink']
 
 @admin.register(Contact)
-class contactAdmin(admin.ModelAdmin):
+class contactAdmin(ImportExportModelAdmin):
     list_display = ['date','name','subject']
 
 @admin.register(Released)
-class releasedAdmin(admin.ModelAdmin):
+class releasedAdmin(ImportExportModelAdmin):
     list_display = ['name','albumartLink','sLink']
-
 @admin.register(EmailList)
-class emailListAdmin(admin.ModelAdmin):
+class emailListAdmin(ImportExportModelAdmin):
     list_display = ['name','email','active']
-    # actions = [download_csv]
     
 @admin.register(HomeScreenYT)
-class homeScreenYTAdmin(admin.ModelAdmin):
+class homeScreenYTAdmin(ImportExportModelAdmin):
     list_display = ['link']
-    
-# admin.site.register(Demosubs)
-# admin.site.register(ImgGallery)
-# admin.site.register(Contact)
-# admin.site.register(Released)
-# admin.site.register(EmailList)
