5f6a3d959396466fa557ba1c9b93096f81efa645
Jash <jashsharma619@gmail.com>
2023-01-25 10:44:13 +0530

    dependencies updated

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,9 @@
-echo " BUILD START"
+echo "BUILD START"
 python - m pip install - r requirements.txt
+echo "RQUIREMENTS INSTALLED"
 python manage.py collectstatic--noinput--clear
+echo "STATIC COLLECTED"
 python manage.py migrate
+echo "DATABASE MIGRATED"
 #web: gunicorn jxsrmaweb.wsgi --log-file -
-echo " BUILD END"
+echo "BUILD END"
